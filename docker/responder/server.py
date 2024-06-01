bf20cd4944fc03643878189d0ef68453faa39f4b
Abhinav <abhi.gupta8802@gmail.com>
2024-06-01 13:26:35 +0530

    docker task done

--- docker/responder/server.py
+++ docker/responder/server.py
@@ -4,10 +4,10 @@ import socket
 
 async def handler(websocket, path):
     while True:
-        message = f"Hello from {socket.gethostbyname(socket.gethostname())}"
-        await websockets.send(message)
+        message = f"Hello from {socket.gethostbyname(socket.gethostname())}!"
+        await websocket.send(message)
         await asyncio.sleep(5)
-    
+
 start_server = websockets.serve(handler, "0.0.0.0", 8000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
